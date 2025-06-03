88d2663975670fae3ca317a38364bee90a6c07c2
tojadamiansienazywam <116257475+tojadamiansienazywam@users.noreply.github.com>
2025-06-03 11:12:31 +0200

    start.bat

--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\Administrator\Desktop\Plugin BOT
-"C:\Users\Administrator\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\Scripts\streamlit.exe" run main.py
-pause
